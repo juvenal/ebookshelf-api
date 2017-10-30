d10726f8f1330b27ea26fd243d157df027b794ba
Juvenal A. Silva Jr <juvenal.silva.jr@gmail.com>
2017-10-30 10:32:07 -0200

    Updated the MySQL swift native to 1.4

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "1.7.9"),
         .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", from: "1.7.1"),
-        .package(url: "https://github.com/mcorega/MySqlSwiftNative.git", from: "1.3.1"),
+        .package(url: "https://github.com/mcorega/MySqlSwiftNative.git", from: "1.4.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
